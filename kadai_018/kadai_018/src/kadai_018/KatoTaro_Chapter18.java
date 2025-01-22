f5b52566a95c404a87d884a8d0d0581376132f64
otsukieri11889 <otsukieri0610102@gmail.com>
2025-01-22 17:11:18 +0900

    Add files via upload

---
+++ kadai_018/kadai_018/src/kadai_018/KatoTaro_Chapter18.java
@@ -0,0 +1,18 @@
+package kadai_018;
+
+public class KatoTaro_Chapter18 extends Kato_Chapter18{
+	
+
+	// コンストラクタの追加
+    public KatoTaro_Chapter18() {
+        setGivenName("太郎"); // ここで名前をセット
+    }
+
+    //Override
+	public void eachIntroduce() {
+        System.out.println("私はKJavaが得意です");
+    }
+
+    
+	
+}
---
+++ kadai_018/kadai_018/src/kadai_018/Kato_Chapter18.java
@@ -0,0 +1,35 @@
+package kadai_018;
+
+public class Kato_Chapter18 {
+//親クラス
+	
+	//フィールド
+	String familyName ="加藤";
+	String givenName;
+	String address ="東京都中野区〇×";
+
+	//メソッド
+	//共通の紹介を出力する
+	public  void outputcommonIntroduce() {
+	 System.out.println("名前は" + familyName + givenName + "です");
+	 System.out.println("住所は" + address + "です");
+	 
+
+    }
+
+    //個別の紹介を出力する
+ 	public void eachIntroduce() {
+		
+	}
+ 	
+ 	//紹介を実行する
+	public void execIntroduce() {
+		outputcommonIntroduce();
+		eachIntroduce();
+	} 
+	
+	 // ローカル変数ではなく、クラスのフィールドに値をセット
+    void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+}
